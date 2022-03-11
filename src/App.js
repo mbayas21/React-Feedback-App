52d27d867fb7718e486e7da9935f73f7dddb47df
Mauricio Bayas <mbayas1993@gmail.com>
2022-03-11 18:04:38 -0500

    Added Routing v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from "uuid";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import { useState } from "react";
 import "./data/FeedbackData";
@@ -6,28 +7,45 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./components/pages/AboutPage";
+import AboutIconLink from "./components/pages/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
-  
+
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback,...feedback]);
-  }
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
+  };
 
   return (
     <>
-      <Header text="Hello World" />
+      <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink  />
       </div>
     </>
   );
