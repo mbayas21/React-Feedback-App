4c67d8348c603a5955db2fa3dd85f785f24313a4
Mauricio Bayas <mbayas1993@gmail.com>
2022-03-23 16:33:46 -0400

    Add feedback item json server functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -18,19 +18,27 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response =  await fetch(`http://localhost:5000/feedback?_sort=id&_order=asc`);
-    const data = await response.json();
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
 
-    setFeedback(data);
+    setFeedback(data)
     setIsLoading(false)
-
   }
 
   // Add feedback item
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
+  }
 
   // Delete feedback item
   const deleteFeedback = (id) => {
